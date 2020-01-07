e7ec8b5fc68c1bd741101dce903029f86bb13814
Laura Collins <laura.e.collins1@gmail.com>
2020-01-06 18:06:38 -0600

    base auth set up

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Redirect, Switch,
 } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
+
+import firebaseConnection from '../helpers/data/connection';
 import Home from '../components/pages/Home/Home';
 import Auth from '../components/pages/Auth/Auth';
 import SingleBoard from '../components/pages/SingleBoard/SingleBoard';
@@ -18,10 +23,26 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return <Route {...rest} render={(props) => routeChecker(props)} />;
 };
 
+firebaseConnection();
+
 class App extends React.Component {
   state = {
-    authed: true,
+    authed: false,
   };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ authed: true});
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillMount() {
+    this.removeListener();
+  }
 
   render() {
     const { authed } = this.state;
