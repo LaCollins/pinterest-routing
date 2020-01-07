a8f45189e5c125e7117dff0a7da1f488bc892b76
Laura Collins <laura.e.collins1@gmail.com>
2020-01-06 18:18:17 -0600

    navbar base setup

---
+++ src/components/shared/MyNavBar/MyNavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyNavBar extends React.Component {
+  render() {
+    return (
+      <div className="MyNavBar">
+        <h1>Navbar</h1>
+      </div>
+    );
+  }
+}
+
+export default MyNavBar;
