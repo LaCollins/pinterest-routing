8920bd3fe3fbefe31dd3423f31ce67f1c7aa7d96
Laura Collins <laura.e.collins1@gmail.com>
2020-01-07 21:16:39 -0600

    delete works

--- src/components/shared/Board/Board.js
+++ src/components/shared/Board/Board.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import boardShape from '../../../helpers/propz/boardShape';
 
@@ -8,6 +8,13 @@ import './Board.scss';
 class Board extends React.Component {
   static propTypes = {
     board: boardShape.boardShape,
+    deleteBoard: PropTypes.func,
+  }
+
+  deleteBoardEvent = (e) => {
+    e.preventDefault();
+    const { deleteBoard, board } = this.props;
+    deleteBoard(board.id);
   }
 
   render() {
@@ -17,7 +24,10 @@ class Board extends React.Component {
       <div className="Board col-4">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">{board.name}</h5>
+            <div className="d-flex justify-content-end">
+              <button className="btn btn-danger m-0" onClick={this.deleteBoardEvent}>X</button>
+            </div>
+            <h5 className="card-title m-0">{board.name}</h5>
             <p className="card-text">{board.description}</p>
           </div>
             <div className="card-footer">
