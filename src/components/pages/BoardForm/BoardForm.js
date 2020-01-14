f65ac8bd05286b7a939b090069ef1192e64374e7
Laura Collins <laura.e.collins1@gmail.com>
2020-01-13 21:03:16 -0600

    edit works

--- src/components/pages/BoardForm/BoardForm.js
+++ src/components/pages/BoardForm/BoardForm.js
@@ -43,8 +43,22 @@ class BoardForm extends React.Component {
       .catch((error) => console.error('err from save board', error));
   }
 
+  editBoardEvent = (e) => {
+    e.preventDefault();
+    const { boardId } = this.props.match.params;
+    const updatedBoard = {
+      name: this.state.boardName,
+      description: this.state.boardDescription,
+      uid: authData.getUid(),
+    };
+    boardData.updateBoard(boardId, updatedBoard)
+      .then(() => this.props.history.push('/'))
+      .catch((error) => console.error('err from edit board', error));
+  }
+
   render() {
     const { boardName, boardDescription } = this.state;
+    const { boardId } = this.props.match.params;
     return (
       <form className="BoardForm">
         <div className="form-group">
@@ -69,7 +83,11 @@ class BoardForm extends React.Component {
             >
             </input>
         </div>
-        <button className="btn btn-dark" onClick={this.saveBoardEvent}>Save Board</button>
+        {
+          boardId
+            ? (<button className="btn btn-dark" onClick={this.editBoardEvent}>Edit Board</button>)
+            : (<button className="btn btn-dark" onClick={this.saveBoardEvent}>Save Board</button>)
+        }
       </form>
     );
   }
